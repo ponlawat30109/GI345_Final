230e9bb651982e222fa4e76e55995cdf2712e2bd
ponlawat suwanyukabordin <ponlawat30109@users.noreply.github.com>
2022-04-25 23:13:59 +0700

    Update Room

---
+++ Assets/Scripts/Room/DoorBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    // void Start()
+    // {
+    //     Collider[] colliders = Physics.OverlapSphere(transform.position, 0.01f);
+
+
+    //     foreach (Collider collider in colliders)
+    //     {
+    //         Debug.Log(collider.name);
+    //         if (collider.tag == "Wall")
+    //         {
+
+    //             Destroy(gameObject);
+    //             return;
+    //         }
+    //     }
+
+    //     GetComponent<Collider>().enabled = true;
+    // }
+}
